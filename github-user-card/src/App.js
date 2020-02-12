e91a24c7076c2efe8b06fc96769f163e1b3c011b
Josef (Joey) Etheridge <56942351+josefeth@users.noreply.github.com>
2020-02-11 17:00:39 -0700

    trying to do stretch

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-
+import UserCard from "./components/UserCard";
+import FollowerCard from "./components/FollowersCard";
+import UserForm from './components/UserForm';
 
 
 class App extends React.Component {
@@ -29,11 +30,60 @@ class App extends React.Component {
       })
       .catch(err => console.log(err));
   }
+
+  getUser = (e) => {
+    e.preventDefault();
+    const user = e.target.elements.username.value;
+    if (user) {
+      axios.get(`https://api.github.com/users/${user}`)
+      .then((res) => {
+        const repos = res.data.repos_url;
+        const login = res.data.login;
+        const bio = res.data.bio;
+        const avatar_url = res.data.avatar_url;
+        const location = res.data.location;
+        this.setState({ repos });
+        this.setState({ login });
+        this.setState({ bio });
+        this.setState({ avatar_url });
+        this.setState({ location });
+      })
+    } else return;
+  }
+
+
   render() {
     return (
       <div className="App">
+        <UserForm getUser={this.getUser} />
+        {this.state.repos}<br></br>
+        {this.state.login}<br></br>
+        {this.state.bio}<br></br>
+        {this.state.avatar_url}<br></br>
+        {this.state.location}<br></br>
+       
         <h1>GitHub User</h1>
-        
+        {this.state.user.map(item => (
+          <UserCard
+            key={item.id}
+            name={item.name}
+            login={item.login}
+            bio={item.bio}
+            img={item.avatar_url}
+            url={item.html_url}
+            location={item.location}
+          />
+        ))} 
+        <h2>Followers:</h2>
+        <div className="followers">
+          {this.state.followers.map(item => (
+            <FollowerCard
+              key={item.id}
+              login={item.login}
+              img={item.avatar_url}
+            />
+          ))}
+        </div>
       </div>
     );
   }
